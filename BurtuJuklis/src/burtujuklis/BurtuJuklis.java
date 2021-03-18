96bff25c02dc5f2468ff36d1696c82acee135243
Dārta Gobleja <80467516+gdarta@users.noreply.github.com>
2021-03-18 12:59:30 +0200

    Update BurtuJuklis.java

--- BurtuJuklis/src/burtujuklis/BurtuJuklis.java
+++ BurtuJuklis/src/burtujuklis/BurtuJuklis.java
@@ -9,120 +9,128 @@ import java.util.Random;
  * @author darta
  */
 public class BurtuJuklis {
-    private String[][] burtuJuklis; //masivs kur kartot vardus
-    private String burts; 
-    private String [] burti;
-    private int lielums;
-public BurtuJuklis(){
-    lielums = 20;
-    burtuJuklis = new String[lielums][lielums];
+    private char[][] burtuJuklis; //masivs kur kartot vardus
+    private char[] burti; //masivs atseviskajiem burtiem
+    private int lielums; //burtu jukla lielums
+public BurtuJuklis(int n){
+    lielums = n;
+    burtuJuklis = new char[lielums][lielums];
 }
-public String[] atseviskieBurti(String s){
-   
-    burti = new String[s.length()/2];
-    int k = 0;
-    for (int j = 0; j < s.length(); j++) {//cikls, lai sadalitu vārdu pa burtiem
-       
-        if (j < s.length() - 1) {
-            burts = s.substring(j, j + 2);
-            j = j + 1;
-        } else {
-            burts = s.substring(j); //lai neiet string out of bounds
-        }
-        burti[k] = burts;  
-        k = k +1;
-}
-    return burti; //masivs ar atseviskiem burtiem + atstarpem    
+public char[] atseviskieBurti(String s){ //sadali stringu characteros un izveido masivu
+    char burts;
+    burti = new char[s.length()];
+    for (int j = 0; j < s.length(); j++) {
+       burts = s.charAt(j);
+       burti[j] = burts;  
+    }
+    return burti; //masivs ar atseviskiem burtiem  
 }
 
-public boolean vaiVardsDer(int garums,int virziens_p, int kolona, int rinda){ 
+public boolean vaiVardsDer(int garums,int virziens_p, int rinda, int kolona){ 
 //parbauda vai vards ielien burtu juklis masiva un vai tur ir brivas ailites
-
 boolean tr = true;    
     switch(virziens_p){
         case 0:
        //horizontali
             if(lielums - kolona >= garums){
                 for(int i = 0; i < garums; i++){
-                    if(burtuJuklis[kolona][rinda + i].isEmpty()){
+                    if(burtuJuklis[rinda][kolona + i] == 0){
                         tr = true;
-                    }else break;
+                    }else {
+                        tr = false;
+                        break;
+                    }
+                    
                 }
-        }else tr = false;
+            }else tr = false;
         break;
         case 1:
         //vertikali
             if(lielums - rinda >= garums){
                 for(int i = 0; i < garums; i++){
-                    if(burtuJuklis[kolona + i][rinda].isEmpty()){
+                    if(burtuJuklis[rinda + i][kolona] == 0){
                         tr = true;
-                    }else break;
-                }
-        }else tr = false;
+                    }else {
+                        tr = false;
+                        break;
+                    }
+                }    
+            }else tr = false;
         break;
         case 2:
         //diagonali
-            if(rinda + garums <= lielums){
-            for(int i = 0; i < garums; i++){
-                if(burtuJuklis[kolona + i][rinda + i].isEmpty()){
-                    tr = true;
-                }else break;
-            }
-        }else tr = false;
+            if(rinda + garums <= lielums && kolona + garums <= lielums){
+                for(int i = 0; i < garums; i++){
+                    if(burtuJuklis[rinda + i][kolona + i] == 0){
+                        tr = true;
+                    }else {
+                        tr = false;
+                        break;
+                    }
+                }
+            }else tr = false;
     }
     if(tr == true)return true;
     else return false;
 }
-public void rezgaBuvesana(String [] vardiMasiva){
-    
-    String[] burtiMasiva;
+
+public void varduLiksana(char[] burtiMasiva, int virziens, int lauks_rinda, int lauks_kolona){
+    //panem masivu ar burtiem un ieliek burtu jukli
+    for(int j = 0; j < burtiMasiva.length; j++){
+        char viensBurts = burtiMasiva[j];
+        if(virziens == 0){
+            burtuJuklis[lauks_rinda][lauks_kolona + j] = viensBurts;
+        } else if(virziens == 1){
+            burtuJuklis[lauks_rinda + j][lauks_kolona] = viensBurts;
+        } else{
+            burtuJuklis[lauks_rinda + j][lauks_kolona + j] = viensBurts;
+        } 
+    }
+    }
+
+public void rezgaBuvesana(String [] vardiMasiva){ //izveido koordinates vardiem un saliek rezgi
+    char[] burtiMasiva;
     Random r = new Random();
     for(int i = 0; i < vardiMasiva.length; i ++){
-        burtiMasiva = atseviskieBurti(vardiMasiva[i]); // izveido masivu ar burtiem
+        burtiMasiva = atseviskieBurti(vardiMasiva[i]); 
         int virziens = r.nextInt(3);
         int lauks_kolona = (int)(Math.random()*(lielums) + 1); 
-        int lauks_rinda = (int)(Math.random()*(lielums) + 1); 
-        for(int j = 0; j < burtiMasiva.length; j++){
-            burts = burtiMasiva[j];
-           
-                if(vaiVardsDer(burtiMasiva.length, virziens, lauks_kolona, lauks_rinda) == true){
-                    if(virziens == 0){
-                        burtuJuklis[lauks_kolona][lauks_rinda + j] = burts;
-                    } else if(virziens == 1){
-                        burtuJuklis[lauks_kolona + j][lauks_rinda] = burts;
-                    } else{
-                        burtuJuklis[lauks_kolona + j][lauks_rinda + j] = burts;
-                    }
-                }else {
-                    i = i - 1;
-                } break;
-                
+        int lauks_rinda = (int)(Math.random()*(lielums) + 1);
+        if(vaiVardsDer(burtiMasiva.length, virziens, lauks_rinda, lauks_kolona)){
+            varduLiksana(burtiMasiva, virziens, lauks_rinda, lauks_kolona);
+        }else{
+            i = i - 1;
         }
     }
     for (int i = 0; i < burtuJuklis.length; ++i) { // aizpilda tuksas vietas
             for(int j = 0; j < burtuJuklis[i].length; ++j) {
-                if(burtuJuklis[i][j].isEmpty()){
-                    burtuJuklis[i][j] = "x";
+                if(burtuJuklis[i][j] == 0){
+                    burtuJuklis[i][j] = 'x';
                 }
             }
         }
+
 }
     public static void main(String[] args) {
-        BurtuJuklis bj = new BurtuJuklis();
+        BurtuJuklis bj = new BurtuJuklis(20);
         int liel = bj.lielums;
-        String brtj[][] = bj.burtuJuklis;
-        brtj[0][0] = "darta";
+        char brtj[][] = bj.burtuJuklis;
         String[] vm = {
-            "v a r d s ","c i t s-v a r d s ","d a r t a-i r-s t i l i g a "
-    };
-        System.out.println(Arrays.toString(bj.atseviskieBurti("k a k a ")));
-        System.out.println(bj.vaiVardsDer(5, 0, 6, 1));
-        for (int i = 0; i < brtj.length; ++i) { 
-            
-                //bj.rezgaBuvesana(vm);
-                System.out.println(Arrays.toString(brtj[i]));
-            
+            "VARDS","CITS-VARDS","DARTA-IR-STILIGA","SUDS","DABAS-MAJA"
+        }; 
+        bj.rezgaBuvesana(vm);
+//        for(int i = 0; i < vm.length; i++){
+//            char [] atsb = bj.atseviskieBurti(vm[i]);
+//            bj.varduLiksana(atsb, 2, 4, 2);
+        for (int j = 0; j < brtj.length; j++) { 
+            System.out.println(Arrays.toString(brtj[j]));
         }
+//        }
+        System.out.println(Arrays.toString(bj.atseviskieBurti("kaka")));
+        System.out.println(bj.vaiVardsDer(5, 1, liel, liel));
+        System.out.println(bj.vaiVardsDer(5, 0, 0, 0)); 
+        
+        
     }
     
 }
