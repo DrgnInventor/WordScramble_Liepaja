99ae6134015a447eb43f6ed423a935063e183943
gdarta <80467516+gdarta@users.noreply.github.com>
2021-03-26 12:40:25 +0200

    Add files via upload

---
+++ Galvenais.java
@@ -0,0 +1,26 @@
+package burtuJuuklis;
+
+
+public class Galvenais {
+    
+    public static void main(String[] args){
+        Tabula tb = new Tabula();
+        Lasitajs ls = new Lasitajs();
+        BurtuJuklis bj = new BurtuJuklis(10, 26);
+        Adreses ad = new Adreses();
+        Speletajs sp = new Speletajs("Pauls");
+        Interfeis face = new Interfeis();
+
+        char[][] brtj = bj.rezgaBuvesana(ls.lasiVardus(), ad); //izveido rezgi
+        if(bj.tabulaDer == true){//parbauda vai rezgis ir izdevies
+            tb.printTabula(brtj);}
+        System.out.println(bj.tabulaDer);
+        
+        face.x = bj.platums;
+        face.y = bj.garums;
+        
+        sp.sak_laiks(); // speles sakums
+        face.userInput(bj, ad, tb);
+        sp.beigu_laiks(); // speles beigas
+}
+}
