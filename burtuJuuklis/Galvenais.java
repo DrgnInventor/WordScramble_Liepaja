a191778956b89cc0dfb4374af5220aa406232a01
DrgnInventor <62597889+DrgnInventor@users.noreply.github.com>
2021-03-30 19:24:57 +0300

    Pievieno otro vardu ko uztvertu scanner ja mēģina izmantot garumzīmes

--- burtuJuuklis/Galvenais.java
+++ burtuJuuklis/Galvenais.java
@@ -2,7 +2,7 @@ package burtuJuuklis;
 
 public class Galvenais {
     public static void main(String[] args){
-        Spele sp = new Spele("Dabas maja");
+        Spele sp = new Spele("Dabas maja", "Dabas mja");
         sp.gameStartInputs();
         sp.startGame();
         sp.guessName();
