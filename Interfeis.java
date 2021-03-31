8c8b9862e0c7e57521c977f1441021a229de5d84
DrgnInventor <62597889+DrgnInventor@users.noreply.github.com>
2021-03-31 21:18:40 +0300

    Parformatets cerams ka java dievi mus tgd mil

---
+++ Interfeis.java
@@ -0,0 +1,84 @@
+package burtuJuuklis;
+
+import java.util.Scanner;
+
+public class Interfeis {
+	
+    public int x;
+    public int y;
+
+    public void userInput(BurtuJuklis jk, Adreses adr, Tabula t) {
+        Scanner keyboard = new Scanner(System.in);
+        boolean valid = false;
+        char viensburts;
+        char diviburts;
+        int viensskaitlis = 0;
+        int diviskaitlis = 0;
+        int i = adr.rindaVertibam.size();
+
+        do {
+            do {
+                System.out.print("\nIevadi pirmo koordinatu (A1 formata): ");
+                String viens = keyboard.next();
+                viensburts = viens.charAt(0);
+                try {
+                viensskaitlis = Integer.parseInt(viens.substring(1));
+                } catch (NumberFormatException e) {
+                        System.out.println("Koordinata jaievada atbilstosa formata!");
+                        continue;
+                }
+                valid = checkInput(viensburts, viensskaitlis); // parbauda vai koordinata ir pareizi uzrakstita
+            } while (!valid);
+
+            viensburts = Character.toUpperCase(viensburts); // parliecinas ka burts ir pareiza sintaksa
+            valid = false;
+
+            do {
+                System.out.print("Ievadi otro koordinatu (A1 formata): ");
+                String divi = keyboard.next();
+                diviburts = divi.charAt(0);
+                try {
+                        diviskaitlis = Integer.parseInt(divi.substring(1));
+                } catch (NumberFormatException e) {
+                        System.out.println("Koordinata jaievada atbilstosa formata!");
+                        continue;
+                }
+                valid = checkInput(diviburts, diviskaitlis);
+            } while (!valid);
+            diviburts = Character.toUpperCase(diviburts);
+
+            if (adr.salidzinatKoordinates(viensburts, viensskaitlis, diviburts, diviskaitlis)){ //parbauda vai ievaditas koordinates ir pareizas
+                int kartas_nr = adr.kartasNr; //koordinasu indekss vertibu rinda
+                jk.varduDzesana(viensburts, viensskaitlis, diviburts, diviskaitlis, adr); //parvers vardus uz lielajiem burtiem
+                t.printTabula(jk.burtuJuklis); //izprinte tabulu
+                adr.atminetieVardi.add(adr.rindaVertibam.get(kartas_nr)); //pievieno atmineto vardu atmineto vardu rindai
+                System.out.println("\nPareizs vards!\n");
+                System.out.println("Tev vel jaatrod:\n");
+                adr.neatminetieVardi.remove(adr.rindaVertibam.get(kartas_nr).getVardsString());
+                adr.getNeatminetieVardi(); //izprinte neatminetos vardus
+                i--; 
+            } else {
+                System.out.println("\nNav pareizi, megini velreiz!\n");
+                t.printTabula(jk.burtuJuklis); //izprinte tabul
+            }
+
+        } while (i != 0);
+        System.out.println("Tu uzvareji!");
+}
+
+    public boolean checkInput(char burts, int skaitlis) {
+            boolean valid = true;
+            //printTabula?()
+            if (!(Character.isLetter(burts))) { //parbauda vai pirma rakstzime koordinata ir burts
+                    System.out.println("Pirmajai rakstzimei jabut burtam.");
+                    valid = false;
+            } else if (Character.getNumericValue(burts) >= 10 + x) { //parbauda vai burts ietilpst dotajos tabulas izmeros
+                    System.out.println("Burts neieklaujas tabulas koordinatu ietvaros.");
+                    valid = false;
+            } else if (skaitlis <= 0 || skaitlis >= y)  { // parbauda vai cipars ietilpst dotajos tabulas izmeros
+                    System.out.println("Cipars neieklaujas tabulas koordinatu ietvaros.");
+                    valid = false;
+            }
+            return valid;
+    }
+}
