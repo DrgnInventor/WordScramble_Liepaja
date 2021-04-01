9e4b5bf7fb6fcd369f1a3254562ff7eaf6665e9c
DrgnInventor <62597889+DrgnInventor@users.noreply.github.com>
2021-04-01 09:54:50 +0300

    Vestule

---
+++ Vestule.java
@@ -0,0 +1,11 @@
+public class Vestule {
+    
+}
+class Vestule {    
+    public static void main(String[] args) {        
+        System.out.println("Mēs esam komanda Masīvi sākas no 1");        
+        System.out.println("Pielikumā pieejams .zip arhīvs ar visu programmas pirmkodu un saistītajiem failiem.\nVēl ir piesaistīta saite ar programmas GitHub un Replit. \n Github: https://github.com/DrgnInventor/WordScramble_Liepaja \n Replit: https://replit.com/@DrgnInventor/WordScrambleLiepajaMasiviSakasNo1Pabeigtsfinalv99 ");        
+        System.out.println("Kodu ieteicams palaist caur Replit saiti, tādā gadījumā ir tikai nepieciešams uzspiest Play/Run pogu un programma darbojas. \nAlternatīvi, kodu var manuāli palaist cauri CMD, atverot atbilstošo direktoriju, un ierakstot komandu \"java Main\". \nAlternatīvi alternatīvi var kodu kompilēt caur personīgu IDE.");        
+        System.out.println("Ar cieņu, Masīvi sākas no 1");
+        System.out.println("P.S. Priecīgu 1. Aprīli c:");
+    }}
